07945e6bf247e42c79ad921e2af6b3eff9f466d5
CoolCoder2010 <71349048+CoolCoder2010@users.noreply.github.com>
2021-01-17 17:19:06 +0000

    Add files via upload

---
+++ Spawn point.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+"""Dont forget to put in your OWN coordinates!"""
+x = 61
+y = 1
+z = 99
+
+mc.player.setTilePos(x, y, z)
+
+mc.postToChat("Client Message: Respawn point set")
