471cc277c260be5a5db1fcd673ab7278aad5f3dd
CoolCoder2010 <71349048+CoolCoder2010@users.noreply.github.com>
2021-01-17 17:38:50 +0000

    Update Spawn point.py

--- Spawn point.py
+++ Spawn point.py
@@ -1,11 +1,24 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-"""Don`t forget to put in your OWN coordinates!"""
-x = 61
-y = 1
-z = 99
+def yes():
+  """Don`t forget to download and type in your OWN coordinates!"""
+  x = 61
+  y = 1
+  z = 99
+  
+  mc.player.setTilePos(x, y, z)
 
-mc.player.setTilePos(x, y, z)
+  mc.postToChat("Respawn point set")
 
-mc.postToChat("Client Message: Respawn point set")
+answer = input("Continue [Y/n]")
+if answer = "y":
+  yes()
+elif answer = "Y":
+  yes()
+elif answer = "yes":
+  yes()
+elif answer = "Yes":
+  yes()
+elif answer = "YES":
+  yes()
